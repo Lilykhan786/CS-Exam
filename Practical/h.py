0d8d1f224b734c219e467cb730aecdf60eea6457
Lilykhan <47777673+Lilykhan786@users.noreply.github.com>
2021-09-22 18:03:38 +0000

    8th

--- Practical/h.py
+++ Practical/h.py
@@ -1,1 +1,20 @@
 # 8. Write a program to store customer data into a binary file cust.dat using a dictionary and print them on screen after reading them. The customer data contains ID as key, and name, city as values.
+
+import pickle
+rec={}
+def file_create():
+    f = open("cline.dat", "wb")
+    cno = int(input("Enter Clint ID:"))
+    cname = input("Enter Clint Name:")
+    address = input("Enter Address:")
+    rec={cno:[cname,address]}
+    pickle.dump(rec,f)
+def read_data():
+  f= open("cline.dat", "br")
+
+  print("*"*78)
+  print("Data strored in file...")
+  rec=pickle.load(f)
+  for i in rec :
+    print(rec[i])
+file_create()
